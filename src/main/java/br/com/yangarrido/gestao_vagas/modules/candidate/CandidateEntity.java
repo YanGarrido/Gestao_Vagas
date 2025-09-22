518fc0c35e9530c9cf2b88126bac6797eda126ab
Yan <yangarridodev@gmail.com>
2025-09-21 22:24:42 -0300

    feat: adição de algumas validações na entidade Candidato

--- src/main/java/br/com/yangarrido/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/yangarrido/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package br.com.yangarrido.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +12,14 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
   private String username;
+
+  @Email(message = "o campo [email] deve conter um e-mail válido")
   private String email;
+
+  @Length(min = 10, max = 100)
   private String password;
   private String curriculum;
 }
