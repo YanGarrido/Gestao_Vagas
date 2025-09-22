d5fc1f688e8a45b2bdf20de609da19024c79017a
Yan <yangarridodev@gmail.com>
2025-09-22 19:29:17 -0300

    feat: salvando nova company no banco (Company Controller)

---
+++ src/main/java/br/com/yangarrido/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,28 @@
+package br.com.yangarrido.gestao_vagas.modules.company.controllers;
+
+import br.com.yangarrido.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.yangarrido.gestao_vagas.modules.company.userCases.CreateCompanyUserCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+  @Autowired
+  private CreateCompanyUserCase createCompanyUserCase;
+
+  @PostMapping("/")
+  public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity){
+    try{
+      var result = this.createCompanyUserCase.execute(companyEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e){
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+}
