e9dc0051df702143923e60944aae18a5221e97e1
Yan <yangarridodev@gmail.com>
2025-09-21 23:54:02 -0300

    feat: criação do userCase do Usuario

--- src/main/java/br/com/yangarrido/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/yangarrido/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -3,8 +3,10 @@ package br.com.yangarrido.gestao_vagas.modules.candidate.controllers;
 import br.com.yangarrido.gestao_vagas.exceptions.UserFoundException;
 import br.com.yangarrido.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.yangarrido.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.yangarrido.gestao_vagas.modules.candidate.userCases.CreateCandidateUserCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
   @Autowired
-  private CandidateRepository candidateRepository;
+  private CreateCandidateUserCase createCandidateUserCase;
 
   @PostMapping("/")
-  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    this.candidateRepository
-        .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-        .ifPresent((user) -> {
-          throw new UserFoundException();
-        });
+  public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    try{
+      var result = this.createCandidateUserCase.execute(candidateEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 
-    return this.candidateRepository.save(candidateEntity);
   }
 }
