72c29abf9a28b9217c109edaecb1f08d586a7890
yangarrido <yangarridodev@gmail.com>
2025-11-16 20:22:49 -0300

    Refatorando JobController

--- src/main/java/br/com/yangarrido/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/yangarrido/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,13 +1,17 @@
 package br.com.yangarrido.gestao_vagas.modules.company.controllers;
 
+import br.com.yangarrido.gestao_vagas.dto.CreateJobDTO;
 import br.com.yangarrido.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.yangarrido.gestao_vagas.modules.company.userCases.CreateJobUserCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -17,7 +21,18 @@ public class JobController {
   private CreateJobUserCase createJobUserCase;
 
   @PostMapping("/")
-  public JobEntity create(@Valid @RequestBody JobEntity jobEntity){
+  public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
+      var companyID = request.getAttribute("company_id");
+
+      // jobEntity.setCompanyId(UUID.fromString(companyID.toString()));
+
+      var jobEntity = JobEntity.builder()
+              .benefits(createJobDTO.getBenefits())
+              .companyId(UUID.fromString(companyID.toString()))
+              .description(createJobDTO.getDescription())
+              .level(createJobDTO.getLevel())
+              .build();
+
     return this.createJobUserCase.execute(jobEntity);
 
   }
