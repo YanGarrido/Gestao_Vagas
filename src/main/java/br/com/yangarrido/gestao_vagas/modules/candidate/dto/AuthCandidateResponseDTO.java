84bd6d5a1b50cb53711dce23d5d797d9e6f93b8a
yangarrido <yangarridodev@gmail.com>
2025-11-19 14:57:38 -0300

    feat: Criando o token do candidato

---
+++ src/main/java/br/com/yangarrido/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.yangarrido.gestao_vagas.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthCandidateResponseDTO {
+
+    private String access_Token;
+}
