02e5dd46ae54496a533eadc83bd3d750bcc8e4b1
yangarrido <yangarridodev@gmail.com>
2025-12-12 22:12:00 -0300

    Inserindo exemplos nas requisições

--- src/main/java/br/com/yangarrido/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/yangarrido/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,5 +1,6 @@
 package br.com.yangarrido.gestao_vagas.modules.company.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -21,10 +22,14 @@ public class JobEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Schema(example = "Vaga para desenvolvedora backend")
   private String description;
+  @Schema(example = "GYMPASS, VALE REFEIÇÃO")
   private String benefits;
 
   @NotBlank(message = "Esse campo é obrigatório")
+  @Schema(example = "SENIOR")
   private String level;
 
   @ManyToOne()
